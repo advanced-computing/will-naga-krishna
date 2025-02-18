327bcd58e1eb5b4ddc71d31c6a46982350139e8e
Naga <ml1234@columbia.edu>
2025-02-18 14:07:21 -0500

    rename pages and chage the structure

---
+++ Final_Project.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.title('Project Title: Mapping Construction in NYC - Will & Naga')
+
+'''
+Description: This project takes the NYC Department of Construction construction applications
+ and maps them across the city to understand the amount of housing construction the city is
+   undertaking to combat the affordability crisis, where the construction is located and what 
+   type of construction there is.
+'''
