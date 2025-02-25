ac32f0508e4a2452f1301c7ac9216663892cd66d
Naga <ml1234@columbia.edu>
2025-02-25 01:45:12 -0500

    created a function and a pytest file

--- pages/Land_Price.py
+++ pages/Land_Price.py
@@ -1,5 +1,4 @@
 # import libraries
-
 import streamlit as st
 import pandas as pd
 from sodapy import Socrata
@@ -9,7 +8,7 @@ import pydeck as pdk
 st.title("NYC Land Price Mapping - Will & Naga")
 st.write("This is our first attempt at building a map of the land price data in Streamlit")
 
-# reading data
+# read data from api
 client = Socrata("data.cityofnewyork.us", None)
 df_list = []
 offset = 0
@@ -19,19 +18,23 @@ while len(df_list) < 10000:
         offset += 2000
         df_list.extend(results)
 
-# forming dataframe
+# form dataframe
 df_land = pd.DataFrame.from_records(df_list)
-df_land[['sale_price', 'borough', 'latitude', 'longitude']]
+df_land[['sale_price', 'latitude', 'longitude']]
 
-# transforming data
-df_land['sale_price'] = df_land['sale_price'].astype(float)
-df_land['longitude'] = df_land['longitude'].astype(float)
-df_land['latitude'] = df_land['latitude'].astype(float) 
+# create a function to transform data into float
+def to_float(value: pd.Series):
+    return value.astype(float)
+
+# transform data into float
+df_land['sale_price'] = to_float(df_land['sale_price'])
+df_land['latitude'] = to_float(df_land['latitude'])
+df_land['longitude'] = to_float(df_land['longitude'])
 
 # delete the rows including 'Nan'
 df_land.dropna(subset=["latitude", "longitude", "sale_price"], inplace=True)
 
-# Pydeck layer for visualization
+# pydeck layer for visualization
 st.pydeck_chart(
     pdk.Deck(
         map_style=None,
@@ -62,3 +65,5 @@ st.pydeck_chart(
         ],
     )
 )
+
+# data source (https://data.cityofnewyork.us/City-Government/NYC-Citywide-Annualized-Calendar-Sales-Update/w2pb-icbu/about_data)
