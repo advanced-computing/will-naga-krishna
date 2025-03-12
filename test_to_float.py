998bd38cd151960535395c100df163c2c3802d32
Naga <ml1234@columbia.edu>
2025-03-12 15:03:47 -0400

    small modification

--- test_to_float.py
+++ test_to_float.py
@@ -1,6 +1,6 @@
 # import pytest
 import pandas as pd
-from pages.Land_Price import to_float
+from pkg.property_price import to_float
 
 def test_to_float():
     data = pd.Series(['1', '2', '3', '4', '5'])
