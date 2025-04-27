d6bfa6aeb943adf03e88af0e207735949bb31003
Naga <ml1234@columbia.edu>
2025-04-27 17:17:18 -0400

    updated the home page

--- pkg/property_functions.py
+++ pkg/property_functions.py
@@ -15,21 +15,3 @@ def to_residential_type(category):
         return '3 Family'
     else:
         return 'Other'
-
-# def to_borough_name(borough):
-#     if borough == '1':
-#         return 'Manhattan'
-#     elif borough == '2':
-#         return 'Bronx'
-#     elif borough == '3':
-#         return 'Brooklyn'
-#     elif borough == '4':
-#         return 'Queens'
-#     elif borough == '5':
-#         return 'Staten Island'
-#     elif borough == 'MANHATTAN':
-#         return 'Manhattan'
-#     elif borough == 'BRONX':
-#         return 'Bronx'
-#     else:
-#         return 'Unknown'
